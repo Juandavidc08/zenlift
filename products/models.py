2204ae67aa5bd76ed519fa870e192b25c6e91ef9
Juan David Camelo <jdavidcamelo94@gmail.com>
2024-09-17 16:17:12 +0000

    Customized admin panel

--- products/models.py
+++ products/models.py
@@ -1,9 +1,13 @@
 from django.db import models
 
 class Category(models.Model):
-    name = models.CharField(max_length=100)
-    friendly_name = models.CharField(max_length=100, null=True, blank=True)
-    
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+        
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
     def __str__(self):
         return self.name
 
