baaba49dfe1158c6957840bb7c0b656d0f33c039
Juan David Camelo <jdavidcamelo94@gmail.com>
2024-10-02 15:05:30 +0000

    added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -15,6 +15,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
        'order_form': order_form,
+       'stripe_public_key': 'pk_test_51Pt4hYP7w9ayE1nXvcn7oh8Jam2YscoiNFB6SfMKx5oZ9I1xJOfxHNslcCfEjA0KD5Fx1eMb9DaytpzVrDVsev7v00zoguoQaF',
+       'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
