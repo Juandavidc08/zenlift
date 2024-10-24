ad49a98141674b1d18a072ba220124ba4789dd9c
Juan David Camelo <jdavidcamelo94@gmail.com>
2024-10-24 17:10:16 +0000

    added hours and date function in bookings forms

--- bookings/forms.py
+++ bookings/forms.py
@@ -1,10 +1,27 @@
 from django import forms
 from .models import Appointment
+from django.core.exceptions import ValidationError
+from datetime import datetime
 
 class AppointmentForm(forms.ModelForm):
     class Meta:
         model = Appointment
-        fields = ['date', 'time']  # No need to include 'trainer' as we set it programmatically
+        fields = ['date', 'time']
+        widgets = {
+            'date': forms.DateInput(attrs={'type': 'date', 'class': 'form-control'}),
+            'time': forms.TimeInput(attrs={'type': 'time', 'class': 'form-control'}),
+        }
 
-    def __init__(self, *args, **kwargs):
-        super(AppointmentForm, self).__init__(*args, **kwargs)
+    # Validate that the date and time are in the future
+    def clean_date(self):
+        date = self.cleaned_data['date']
+        if date < datetime.now().date():
+            raise ValidationError("You cannot book a past date.")
+        return date
+
+    def clean_time(self):
+        time = self.cleaned_data['time']
+        # Add any other logic to restrict odd hours here
+        if time < datetime.now().time() and self.cleaned_data.get('date') == datetime.now().date():
+            raise ValidationError("The time cannot be in the past.")
+        return time
