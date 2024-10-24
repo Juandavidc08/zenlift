99b41901bff86d4aed31af70b0f0f37b62d97e03
Juan David Camelo <jdavidcamelo94@gmail.com>
2024-10-24 16:37:24 +0000

    uodate views in booking

--- bookings/forms.py
+++ bookings/forms.py
@@ -5,3 +5,6 @@ class AppointmentForm(forms.ModelForm):
     class Meta:
         model = Appointment
         fields = ['trainer', 'date', 'time']
+        widgets = {
+            'trainer': forms.TextInput(attrs={'readonly': 'readonly'}),  # Make the trainer field readonly
+        }
--- bookings/views.py
+++ bookings/views.py
@@ -4,30 +4,31 @@ from .models import Trainer, Appointment
 from .forms import AppointmentForm
 from django.contrib import messages
 
-# List trainers
+# List of Trainers
 def trainers_list(request):
-    trainers = Trainer.objects.all()
+    trainers = Trainer.objects.all()  # Fetch all trainers
     return render(request, 'bookings/trainers_list.html', {'trainers': trainers})
 
-# Book an appointment
+# Book an Appointment
 @login_required
 def book_appointment(request, trainer_id):
-    trainer = get_object_or_404(Trainer, id=trainer_id)
+    trainer = get_object_or_404(Trainer, id=trainer_id)  # Get the selected trainer
     if request.method == 'POST':
         form = AppointmentForm(request.POST)
         if form.is_valid():
             appointment = form.save(commit=False)
-            appointment.user = request.user
-            appointment.trainer = trainer  # Link the appointment to the trainer
+            appointment.user = request.user  # Link the user to the appointment
+            appointment.trainer = trainer  # Assign the trainer automatically
             appointment.save()
             messages.success(request, 'Your appointment has been successfully booked!')
             return redirect('appointment_confirmation', appointment_id=appointment.id)
     else:
-        form = AppointmentForm()
+        # Pre-fill the form with the selected trainer
+        form = AppointmentForm(initial={'trainer': trainer})
 
     return render(request, 'bookings/book_appointment.html', {'form': form, 'trainer': trainer})
 
-# Appointment confirmation
+# Appointment Confirmation
 def appointment_confirmation(request, appointment_id):
-    appointment = get_object_or_404(Appointment, id=appointment_id)
+    appointment = get_object_or_404(Appointment, id=appointment_id)  # Get the appointment details
     return render(request, 'bookings/appointment_confirmation.html', {'appointment': appointment})
