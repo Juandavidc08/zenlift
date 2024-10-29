6c56f23817df8352efdb2ec506a76ed2e24b81eb
Juan David Camelo <jdavidcamelo94@gmail.com>
2024-10-29 20:59:50 +0000

    email check

--- zenlift/settings.py
+++ zenlift/settings.py
@@ -230,7 +230,7 @@ STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    DEFAULT_FROM_EMAIL ='zenlift@customersupport.com'
+    DEFAULT_FROM_EMAIL ='siedesignsolution@gmail.com'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
