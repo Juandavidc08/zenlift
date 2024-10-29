7450eb83be5a742a94f78631452955b4e110e4ff
Juan David Camelo <jdavidcamelo94@gmail.com>
2024-10-29 12:33:59 +0000

    update urls in bookings

--- bookings/urls.py
+++ bookings/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from .views import update_appointment, delete_appointment
 
 urlpatterns = [
     path('', views.trainers_list, name='trainers_list'),
